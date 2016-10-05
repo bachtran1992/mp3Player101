dd4ed9c216ac77b5a5ea1696f08f8aa022b47fa3
bachtran1992 <bachtran1992@gmail.com>
2016-10-05 16:35:34 +0700

    Quick Player

--- mp3Player101/ViewController.swift
+++ mp3Player101/ViewController.swift
@@ -7,19 +7,107 @@
 //
 
 import UIKit
+import AVFoundation
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var btn_Play: UIButton!
+    
+    @IBOutlet weak var lbl_TimeTotal: UILabel!
+    @IBOutlet weak var lbl_TimeLeft: UILabel!
+    var audio = AVAudioPlayer()
+    var Playing = true
+    @IBOutlet weak var sld_Volume: UISlider!
+    @IBOutlet weak var sld_Duration: UISlider!
+    
+    @IBOutlet weak var switch_RepeatOrNot: UISwitch!
+    var repeat101: Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        audio = try! AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: Bundle.main.path(forResource: "music",ofType: ".mp3")!) as URL)
+        audio.prepareToPlay()
+        addThumbImgForSlider()
+        repeat101 = true
+        
+        audio.currentTime = 0
+    
+        audio.delegate = self
+        audio.numberOfLoops =  -1
+
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeLeft), userInfo: nil, repeats: true)
+        
+        
+        let timer2 = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeTotal), userInfo: nil, repeats: false)
+
+        
+        self.sld_Duration.value = Float(audio.currentTime/audio.duration)
+        
+    }
+    func updateTimeLeft()
+    {
+        self.lbl_TimeLeft.text = String(format: "%2.2f",audio.currentTime/60)
+        self.sld_Duration.value = Float(audio.currentTime/audio.duration)
+        
+           }
+
+    func updateTimeTotal()
+    {
+        self.lbl_TimeTotal.text = String(format: "%2.2f",audio.duration/60 )
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func addThumbImgForSlider()
+    {
+        sld_Volume.setThumbImage(UIImage(named: "thumb.png"), for: .normal)
+        sld_Volume.setThumbImage(UIImage(named: "thumbHightLight.png"), for: .highlighted)
+    }
+  
+    @IBAction func sld_Volume(_ sender: UISlider) {
+        audio.volume = sender.value
     }
 
+    @IBAction func sld_WhenToPlay(_ sender: UISlider) {
+     audio.currentTime = TimeInterval(sender.value) * audio.duration
+        print(sender.value)
+    }
 
+    
+    
+    @IBAction func switch_RepeatOrNot(_ sender: AnyObject) {
+        
+        if (repeat101 == true){
+            audio.numberOfLoops =  0
+           repeat101 = false
+        }
+        else
+        {
+            audio.numberOfLoops =  -1
+            repeat101 = true
+        }
+        
+    }
+    
+  
+
+    @IBAction func action_Play(_ sender: AnyObject) {
+        if (Playing == true)
+        {
+        audio.play()
+            btn_Play.setImage(UIImage(named: "pause.png"), for: UIControlState())
+        }
+        else{
+            audio.pause()
+            btn_Play.setImage(UIImage(named: "play.png"), for: UIControlState())
+        }
+    Playing = !Playing
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if (repeat101 == false)
+        {
+            btn_Play.setImage(UIImage(named: "play.png"), for: UIControlState())
+        }
+        }
+    
 }
+
+
 
